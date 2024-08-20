65579b1ed0e6a5cbb12e9a91d66674e3b216ce11
Brayden <bmassa56@gmail.com>
2024-08-20 19:26:47 -0400

    tweaking responsiveness

--- server/server.js
+++ server/server.js
@@ -1,19 +1,19 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
-require('dotenv').config()
-const shiftRoutes = require('./routes/ShiftRoutes')
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+const shiftRoutes = require("./routes/ShiftRoutes");
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.use('/shifts', shiftRoutes)
+app.use("/shifts", shiftRoutes);
 
-mongoose.connect(process.env.DB_URI).then(() => console.log('DB connected'))
+mongoose.connect(process.env.DB_URI).then(() => console.log("DB connected"));
 
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 4000;
 app.listen(process.env.PORT || 4000, () =>
   console.log(`Server running on port ${PORT}`)
-)
+);
